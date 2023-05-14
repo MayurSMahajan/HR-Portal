b8251709fdd1f5c888f59c0b03c76066fd82df75
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-05-14 16:52:22 +0530

    chore: add margin between postings

--- client/src/components/home/JobPostingsContent.js
+++ client/src/components/home/JobPostingsContent.js
@@ -10,6 +10,22 @@ const mockActiveJobsList = [
     "applicants":"121",
     "shortlisted": "15",
     "interviewed":"6"
+  },
+  {
+    "title": "Senior Software Fullstack Engineer",
+    "date": "05/05/2023",
+    "budget": "$50k - $75k",
+    "applicants":"121",
+    "shortlisted": "15",
+    "interviewed":"6"
+  },
+  {
+    "title": "Senior Software Fullstack Engineer",
+    "date": "05/05/2023",
+    "budget": "$50k - $75k",
+    "applicants":"121",
+    "shortlisted": "15",
+    "interviewed":"6"
   }
 ];
 
