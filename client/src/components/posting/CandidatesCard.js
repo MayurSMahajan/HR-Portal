179aad60478b7c4126fce256dabb3a7bb1539b3f
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-05-20 22:30:10 +0530

    feat: candidate card ui

---
+++ client/src/components/posting/CandidatesCard.js
@@ -0,0 +1,56 @@
+import React from "react";
+import goldBadgeIcon from '../../assets/icons/gold_badge.png';
+import silverBadgeIcon from '../../assets/icons/silver_badge.png';
+import bronzeBadgeIcon from '../../assets/icons/bronze_badge.png';
+
+const CandidatesCard = () => {
+  const badges = {
+    "gold": ["Java"],
+    "silver": ["Android"],
+    "bronze": []
+  }
+
+  return (
+    <div className="candidates-card">
+      <p>Mayur Mahajan</p>
+      <div className="candidate-info-container">
+        <p className="candidate-card-key">Experience</p>
+        <p className="candidate-card-value">2 Years</p>
+      </div>
+      <div className="candidate-info-container">
+        <p className="candidate-card-key">Current Job</p>
+        <p className="candidate-card-value">SDE - 2 Mozilla</p>
+      </div>
+      <p className="candidate-card-key">Badges</p>
+      <div className="candidate-info-container">
+        <div className="badges-container">
+          {badges.gold.map((item) => (
+            <BadgeCard image={goldBadgeIcon} name={item}/>
+          ))}
+        </div>
+        <div className="badges-container">
+          {badges.silver.map((item) => (
+            <BadgeCard image={silverBadgeIcon} name={item}/>
+          ))}
+        </div>
+        <div className="badges-container">
+          {badges.bronze.map((item) => (
+            <BadgeCard image={bronzeBadgeIcon} name={item}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const BadgeCard = ({image, name}) => { 
+  return (
+    <div className="badge-card">
+      <img src={image} alt="badge" className="badge-icon"/>
+      <p>{name}</p>
+
+    </div>
+  );
+}
+
+export default CandidatesCard;
--- client/src/components/posting/PostingDetail.js
+++ client/src/components/posting/PostingDetail.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./posting.css";
 import PostingDetailCard from "./PostingDetailCard";
 import mockActiveJobsList from "../../mock_data/mockActiveJobsList";
+import CandidatesCard from "./CandidatesCard";
 
 const PostingDetail = () => {
   return (
       <div className="postings_container">
         <h3 className="postings-heading">Postings Detail</h3>
         <PostingDetailCard {...(mockActiveJobsList[0])} />
+        <div className="candidates-section">
+          <h3 className="postings-heading">Interviewed Candidates</h3>
+          <div className="candidates-container">
+            <CandidatesCard/>
+          </div>
+        </div>
       </div>
   );
 };
