d9842683b6095975da0628ec1566c2659ae6b0be
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-05-21 23:09:46 +0530

    feat: add more candidates section

--- client/src/components/posting/PostingDetail.js
+++ client/src/components/posting/PostingDetail.js
@@ -3,19 +3,54 @@ import "./posting.css";
 import PostingDetailCard from "./PostingDetailCard";
 import mockActiveJobsList from "../../mock_data/mockActiveJobsList";
 import CandidatesCard from "./CandidatesCard";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 const PostingDetail = () => {
   return (
-      <div className="postings_container">
-        <h3 className="postings-heading">Postings Detail</h3>
-        <PostingDetailCard {...(mockActiveJobsList[0])} />
-        <div className="candidates-section">
+    <div className="postings_container">
+      <h3 className="postings-heading">Postings Detail</h3>
+      <PostingDetailCard {...mockActiveJobsList[0]} />
+      <div className="candidates-section">
+        <div className="candidates-type-container">
           <h3 className="postings-heading">Interviewed Candidates</h3>
-          <div className="candidates-container">
-            <CandidatesCard/>
-          </div>
+        </div>
+        <div className="candidates-container">
+          <CandidatesCard />
+          <CandidatesCard />
+          <CandidatesCard />
+          <ViewAllCard />
+        </div>
+        <div className="candidates-type-container">
+          <h3 className="postings-heading">Shortlisted Candidates</h3>
+        </div>
+        <div className="candidates-container">
+          <CandidatesCard />
+          <CandidatesCard />
+          <CandidatesCard />
+          <ViewAllCard />
+        </div>
+        <div className="candidates-type-container">
+          <h3 className="postings-heading">Top Candidates</h3>
+        </div>
+        <div className="candidates-container">
+          <CandidatesCard />
+          <CandidatesCard />
+          <CandidatesCard />
+          <ViewAllCard />
         </div>
       </div>
+    </div>
+  );
+};
+
+const ViewAllCard = () => {
+  return (
+    <div className="view-all-card">
+      <div className="view-all-btn">
+        <p className="view-all-text"> View All</p>
+        <ArrowOutwardIcon className="view-all-icon" />
+      </div>
+    </div>
   );
 };
 
