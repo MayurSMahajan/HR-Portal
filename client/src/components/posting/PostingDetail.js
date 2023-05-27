613e5f5029f597592d632f49cbc704bd70025f60
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-05-27 14:03:45 +0530

    refactor: add view all route

--- client/src/components/posting/PostingDetail.js
+++ client/src/components/posting/PostingDetail.js
@@ -4,8 +4,17 @@ import PostingDetailCard from "./PostingDetailCard";
 import mockActiveJobsList from "../../mock_data/mockActiveJobsList";
 import CandidatesCard from "./CandidatesCard";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import { useNavigate } from "react-router-dom";
 
 const PostingDetail = () => {
+
+  const navigate = useNavigate();
+
+  const navigateToAllCandidates = () => {
+    console.log("Heya!")
+    navigate('/posting/all');
+  }
+
   return (
     <div className="postings_container">
       <h3 className="postings-heading">Postings Detail</h3>
@@ -18,7 +27,7 @@ const PostingDetail = () => {
           <CandidatesCard />
           <CandidatesCard />
           <CandidatesCard />
-          <ViewAllCard />
+          <ViewAllCard navigateToAllCandidates={navigateToAllCandidates}/>
         </div>
         <div className="candidates-type-container">
           <h3 className="postings-heading">Shortlisted Candidates</h3>
@@ -27,9 +36,9 @@ const PostingDetail = () => {
           <CandidatesCard />
           <CandidatesCard />
           <CandidatesCard />
-          <ViewAllCard />
+          <ViewAllCard navigateToAllCandidates={navigateToAllCandidates}/>
         </div>
-        <div className="secondary-btn-container">
+        <div className="secondary-btn-container" onClick={navigateToAllCandidates}>
           <span className="secondary-btn">View More Candidates</span>
         </div>
       </div>
@@ -37,9 +46,10 @@ const PostingDetail = () => {
   );
 };
 
-const ViewAllCard = () => {
+const ViewAllCard = ({navigateToAllCandidates}) => {
+  
   return (
-    <div className="view-all-card">
+    <div className="view-all-card" onClick={navigateToAllCandidates}>
       <div className="view-all-btn">
         <p className="view-all-text"> View All</p>
         <ArrowOutwardIcon className="view-all-icon" />
