951802a984ab476d87b4d426526e449807a944f2
onkar kotule <kotuleonkar101@gmail.com>
2023-05-27 14:54:35 +0530

    modifications

--- client/src/components/navbar/Header.js
+++ client/src/components/navbar/Header.js
@@ -1,24 +1,62 @@
-import React from "react";
-import "./nav.css";
-import img from "../../assets/icons/img.jpg";
-import notification from "../../assets/icons/noticon.png";
-import { useNavigate } from "react-router-dom";
+import React, { useContext } from 'react'
+import './nav.css'
+import img from '../../assets/img.jpg'
+import notification from '../../assets/icons/noticon.png'
+import { useNavigate } from 'react-router-dom'
+import { DashboardContext, UserContext } from '../../context'
 
 const Header = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="header">
-      <div className="header_logo" onClick={() => navigate("/")}>
-        <img className="logo_img" src={img} alt="logo" />
-        <p>UpLevel</p>
-      </div>
+    const { user } = useContext(UserContext)
+    const { setSection, innerWidth, sideBarToggel, setSideBarToggel } = useContext(DashboardContext)
+    const navigate = useNavigate()
 
-      <div className="header_right">
-        <img className="noti_img" src={notification} alt="notification" />
-        <img className="profile_img" src={img} alt="profile" /> :
-      </div>
-    </div>
-  );
-};
+    const handelClick = (val, route) => {
+        sessionStorage.setItem('currentsection', val)
+        setSection(val)
+        navigate(route)
+    }
 
-export default Header;
+    const handelSiderbar = () => {
+        setSideBarToggel(!sideBarToggel)
+    }
+    return (
+        < >
+            {innerWidth <= 800 ?
+                <div className='header'>
+
+                    <button className='toggle_button' onClick={() => handelSiderbar()}> ok</button>
+                    <div className='header_logo' onClick={() => handelClick(0, '/')}>
+                        <img className='logo_img' src={img} alt="logo" />
+                        <p>UpLevel</p>
+                    </div>
+                </div>
+
+                :
+                <div className='header'>
+
+                    <div className='header_logo' onClick={() => handelClick(0, '/')}>
+                        <img className='logo_img' src={img} alt="logo" />
+                        <p>UpLevel</p>
+                    </div>
+                    <div className='header_right'>
+                        <img className='noti_img' src={notification} alt="notification" />
+                        {user ? <img className='profile_img' src={img} alt="profile" /> :
+                            <div>
+                                <button className='btn_1' onClick={() => navigate('/login')}>signup</button>
+                                {/* <button className='btn_2' onClick={() => navigate('/register')}>signin</button> */}
+                            </div>
+                        }
+                    </div>
+                </div>
+            }
+
+
+
+
+
+
+        </>
+    )
+}
+
+export default Header
--- client/src/components/navbar/Sidebar.js
+++ client/src/components/navbar/Sidebar.js
@@ -1,39 +1,93 @@
 import React, { useContext } from 'react'
 import './nav.css'
 import homeicon from '../../assets/icons/homeicon.png'
+import profile from '../../assets/icons/profileicon.png'
 import job from '../../assets/icons/jobsicon.png'
 import messageicon from '../../assets/icons/messageicon.png'
 import { useNavigate } from 'react-router-dom'
+import { useRef } from 'react'
+import { useEffect } from 'react'
 import { DashboardContext } from '../../context'
 
 const Sidebar = () => {
-
-    const { section, setSection } = useContext(DashboardContext)
+    const { section, setSection, innerWidth, sideBarToggel, setSideBarToggel } = useContext(DashboardContext)
     const navigate = useNavigate()
 
-    const handleClick = (val, route) => {
+    const handelClick = (val, route) => {
+        sessionStorage.setItem('currentsection', val)
         setSection(val)
-        console.log(section)
         navigate(route)
     }
+
+    console.log(sideBarToggel)
+    const sidebarRef = useRef(null);
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (
+                sidebarRef.current &&
+                !sidebarRef.current.contains(event.target) &&
+                !event.target.classList.contains('toggle_button')
+            ) {
+                setSideBarToggel(false);
+            }
+        };
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, [setSideBarToggel]);
     return (
-        <nav className='side_nav'>
-            <div className='sidenav_items_div'>
-                <button onClick={() => handleClick(0, '/home')} className={section === 0 ? ' nav_item active' : 'nav_item'}>
-                    <img className='side_nav_img' src={homeicon} alt='logo' />
-                    <h3>Home</h3>
-                </button>
-                <button onClick={() => handleClick(2, '/job')} className={section === 2 ? ' nav_item active' : 'nav_item'}>
-                    <img className='side_nav_img' src={job} alt='logo' />
-                    <h3>Jobs</h3>
-                </button>
-                <button onClick={() => handleClick(3, '/messages')} className={section === 3 ? ' nav_item active' : 'nav_item'}>
-                    <img className='side_nav_img' src={messageicon} alt='logo' />
-                    <h3>Messaging</h3>
-                </button>
-            </div>
-        </nav>
+        <>
+
+            {innerWidth <= 800 ?
+                <nav ref={sidebarRef} className={sideBarToggel ? 'visible collapse_sidebar' : " none"}>
+                    <div className='sidenav_items_div'>
+                        <button onClick={() => handelClick(0, '/home')} className={section === 0 ? ' nav_item active' : 'nav_item'}>
+                            <img className='side_nav_img' src={homeicon} alt='logo' />
+                            <h3>Home</h3>
+                        </button>
+
+                        <button onClick={() => handelClick(1, '/job')} className={section === 1 ? ' nav_item active' : 'nav_item'}>
+                            <img className='side_nav_img' src={job} alt='logo' />
+                            <h3>Jobs</h3>
+                        </button>
+                        <button onClick={() => handelClick(2, '/messages')} className={section === 2 ? ' nav_item active' : 'nav_item'}>
+                            <img className='side_nav_img' src={messageicon} alt='logo' />
+                            <h3>Messaging</h3>
+                        </button>
+                    </div>
+                    <div >
+                        <p>Refer and Win</p>
+                    </div>
+                </nav> :
+                <nav className=" side_nav">
+                    <div className='sidenav_items_div'>
+                        <button onClick={() => handelClick(0, '/home')} className={section === 0 ? ' nav_item active' : 'nav_item'}>
+                            <img className='side_nav_img' src={homeicon} alt='logo' />
+                            <h3>Home</h3>
+                        </button>
+
+                        <button onClick={() => handelClick(1, '/job')} className={section === 1 ? ' nav_item active' : 'nav_item'}>
+                            <img className='side_nav_img' src={job} alt='logo' />
+                            <h3>Jobs</h3>
+                        </button>
+                        <button onClick={() => handelClick(2, '/messages')} className={section === 2 ? ' nav_item active' : 'nav_item'}>
+                            <img className='side_nav_img' src={messageicon} alt='logo' />
+                            <h3>Messaging</h3>
+                        </button>
+                    </div>
+                    <div >
+                        <p>Refer and Win</p>
+                    </div>
+                </nav>
+            }
+        </>
     )
 }
 
 export default Sidebar
+
+
