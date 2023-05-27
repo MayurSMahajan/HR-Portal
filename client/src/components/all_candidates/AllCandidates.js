76dfdc780a6a15e71a88a5b3df10d722cd9329fd
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-05-27 20:24:10 +0530

    style: all candidates page

--- client/src/components/all_candidates/AllCandidates.js
+++ client/src/components/all_candidates/AllCandidates.js
@@ -1,18 +1,40 @@
 import React from "react";
-import HorizontalCandidateCard from "./HorizontalCandidateCard";
-import './all_candidates.css';
+import "./all_candidates.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CandidatesCard from "../posting/CandidatesCard";
+import { useNavigate } from "react-router-dom";
 
 const AllCandidates = () => {
+    const navigate = useNavigate();
+
+    const navigateBack = () => {
+        navigate(-1)
+    }
+
+    const loadCandidateCards = () => {
+        return <CandidateCardContainer/>
+    }
+
+  return (
+    <div className="all-candidate-container">
+      <div className="all-candidate-header">
+        <ArrowBackIcon onClick={navigateBack} sx={{cursor:"pointer"}}/>
+        <p className="all-candidate-heading">Top Candidates with Android, Java and Spring</p>
+      </div>
+      <div className="all-candidate-body">
+        {loadCandidateCards()}
+      </div>
+    </div>
+  );
+};
+
+const CandidateCardContainer = () => {
     return (
-        <div className="all-candidate-container">
-            <div className="all-candidate-heading">
-                <h3>Top Candidates</h3>
-            </div>
-            <div className="all-candidate-body">
-                <div className="all-candidate-card-listview">
-                    <HorizontalCandidateCard/>
-                </div>
-            </div>
+        <div className="candidate-card-container">
+            <CandidatesCard />
+            <CandidatesCard />
+            <CandidatesCard />
+            <CandidatesCard />
         </div>
     );
 }
